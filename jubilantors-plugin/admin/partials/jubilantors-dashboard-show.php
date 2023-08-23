6c2526d3d2215fd024d7e6821da746496ba84956
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-08-23 14:33:51 +0200

    Begin the admin options7

--- jubilantors-plugin/admin/partials/jubilantors-dashboard-show.php
+++ jubilantors-plugin/admin/partials/jubilantors-dashboard-show.php
@@ -11,6 +11,7 @@
     <h1>Modifications de la barre de défilement</h1>
     <label for="percentage">Afficher le pourcentage</label>
     <input type="checkbox" id="percentage" name="jubi-percentage">
+    <input type="color" value="#fad345" name="color">
     <?php
     submit_button('Enregistrer les modifications');
 
@@ -21,19 +22,37 @@
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $jubiPercentage = $_POST['jubi-percentage'];
+    $jubiColor = $_POST['color'];
     if (isset($_POST['jubi-percentage'])) {
+        $jubiPercentage = $_POST['jubi-percentage'];
         if (get_option('jubi-percentage') === false) {
             add_option('jubi-percentage', $jubiPercentage);
+            add_option('jubi-color', $jubiColor);
         } else {
             update_option('jubi-percentage', $jubiPercentage);
+            update_option('jubi-color', $jubiColor);
         }
     } else {
         if (get_option('jubi-percentage') === false) {
             add_option('jubi-percentage', 'off');
+            add_option('jubi-color', $jubiColor);
         } else {
             update_option('jubi-percentage', 'off');
+            update_option('jubi-color', $jubiColor);
         }
     }
 }
+$colorBar = get_option('jubi-color');
+
+if (get_option('jubi-percentage') == 'on'){
+    $percent = '';
+} else {
+    $percent = 'display-none';
+};
+echo '	
+<div class="bar-container">
+    <div class="bar" style="background:' . $colorBar . '">
+        <p class="text-bar ' . $percent . '">50%</p>
+    </div>
+</div>';
 ?>
