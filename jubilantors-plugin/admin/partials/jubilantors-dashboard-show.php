780c14b8ea885ed95c84c9bfeec37a8a10c3dd32
Tobias Doboszewicz <tobias13@hotmail.fr>
2023-08-24 15:38:15 +0200

    create css for panel admin

--- jubilantors-plugin/admin/partials/jubilantors-dashboard-show.php
+++ jubilantors-plugin/admin/partials/jubilantors-dashboard-show.php
@@ -7,30 +7,35 @@ if (get_option('jubi-color') === false) {
 } else {
     $colorBar = get_option('jubi-color');
 }
-if (get_option('jubi-percentage') == 'on'){
+if (get_option('jubi-percentage') == 'on') {
     $percent = '';
 } else {
     $percent = 'display-none';
 }
 ?>
 
+<div class="div-admin">
+    <form action="" method="POST" class="form-admin">
+        <h1>Modifications de la barre de défilement</h1>
+        <div class="check-admin">
+            <label for="percentage">Afficher le pourcentage</label>
+            <input type="checkbox" id="percentage" name="jubi-percentage">
+        </div>
+        <input type="color" value="<?= $colorBar ?>" name="color" id="colorPicker">
+        <?php
+        submit_button('Enregistrer les modifications');
 
-<form action="" method="POST">
-    <h1>Modifications de la barre de défilement</h1>
-    <label for="percentage">Afficher le pourcentage</label>
-    <input type="checkbox" id="percentage" name="jubi-percentage">
-    <input type="color" value="<?=$colorBar?>" name="color">
+        ?>
+    </form>
+
     <?php
-    submit_button('Enregistrer les modifications');
-
-    ?>
-</form>
-
-<?php
-echo '	
-<div class="bar-container">
-    <div class="bar" style="background:linear-gradient(180deg, rgba(0, 0, 0, 1) 0%, ' . $colorBar . ' 50%, rgba(0, 0, 0, 1) 100%)">
-        <p class="text-bar ' . $percent . '">50%</p>
+    echo '	
+<div class="bar-admin">
+    <div class="bar-container">
+        <div class="bar" style="background:linear-gradient(180deg, rgba(0, 0, 0, 1) 0%, ' . $colorBar . ' 50%, rgba(0, 0, 0, 1) 100%)">
+            <p class="text-bar ' . $percent . '">50%</p>
+        </div>
     </div>
-</div>';
-?>
+</div> ';
+    ?>
+</div>
